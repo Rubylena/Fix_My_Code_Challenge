d5fd80da88f841d3b2aedcbefd496c31a0116101
Grace Effiong <graceffiong@gmail.com>
2022-10-11 00:10:26 +0000

    documentation and class capital letter

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
+"""square module"""
 
-class square():
+
+class Square():
+    """Defines a square"""
     
     width = 0
     height = 0
 
     
     def __init__(self, *args, **kwargs):
+        """initialize"""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -15,14 +19,16 @@ class square():
         return self.width * self.width
 
     def permiterOfMySquare(self):
+        """perimeter"""
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """string representation"""
         return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.permiterOfMySquare())
